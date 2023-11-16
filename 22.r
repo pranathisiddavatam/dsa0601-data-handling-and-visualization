1ae89f9771e004dd76baeb41dea7369657a5a477
pranathisiddavatam <113352841+pranathisiddavatam@users.noreply.github.com>
2023-11-16 09:15:59 +0530

    Update 22.r

--- 22.r
+++ 22.r
@@ -1,5 +1,4 @@
-library(ggplot2)
-png(file = &quot;scatterplot3.png&quot;)
-ggplot(mtcars, aes(x = log(mpg), y = log(drat))) +geom_point(aes(color = factor(gear))) +
-stat_smooth(method = &quot;lm&quot;,col = &quot;
+library(ggplot2) 
+png(file = "scatterplot3me.png")  
+ggplot(mtcars, aes(x = log(mpg), y = log(drat))) +geom_point(aes(color = factor(gear))) + stat_smooth(method = "lm",col = "#C42126",se = FALSE,size = 1)  .  
 dev.off()
