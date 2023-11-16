7c9e97a8339561116a20cf1d3957ca22f103278b
pranathisiddavatam <113352841+pranathisiddavatam@users.noreply.github.com>
2023-11-16 09:17:09 +0530

    Update 24.r

--- 24.r
+++ 24.r
@@ -1,12 +1,12 @@
-library(ggplot2)
-png(file = &quot;scatterplot6.png&quot;)
-new_graph&lt;-ggplot(mtcars, aes(x = log(mpg), y = log(drat))) +geom_point(aes(color = factor(gear))) +
-stat_smooth(method = &quot;lm&quot;,col = &quot;
-new_graph + labs(
-title =
-&quot;Relation between Mile per hours and drat&quot;,
-subtitle =
-&quot;Relationship break down by gear class&quot;,
-caption = &quot;Authors own computation&quot;
-)
-dev.off()
+library(ggplot2)  
+png(file = "scatterplot6me.png")     
+new_graph<-ggplot(mtcars, aes(x = log(mpg), y = log(drat))) +geom_point(aes(color = factor(gear))) +  
+stat_smooth(method = "lm",col = "#C42126",se = FALSE,size = 1)  
+new_graph + labs(  
+        title =  
+                "Relation between Mile per hours and drat",  
+        subtitle =  
+                "Relationship break down by gear class",  
+        caption = "Authors own computation"  
+)  
+dev.off()  
