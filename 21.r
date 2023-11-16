206988dc301233ab4a83028dbfbcceb8fc182094
pranathisiddavatam <113352841+pranathisiddavatam@users.noreply.github.com>
2023-11-16 09:15:17 +0530

    Update 21.r

--- 21.r
+++ 21.r
@@ -1,4 +1,4 @@
-library(ggplot2)
-png(file = &quot;scatterplot2.png&quot;
-ggplot(mtcars, aes(x = log(mpg), y = log(drat))) +geom_point(aes(color=factor(gear)))
-dev.off()
+library(ggplot2)   
+png(file = "scatterplot2me.png")   
+ggplot(mtcars, aes(x = log(mpg), y = log(drat))) +geom_point(aes(color=factor(gear)))   
+dev.off()  
